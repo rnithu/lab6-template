bdd008b9329e41898c3bd76ba474b4b08b3e8d85
Nithurshan Raveendran <rnaveendran5@myseneca.ca>
2024-11-08 03:16:31 -0500

    Lab 6 Completed

---
+++ lab6a.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+# Any line that starts with a "#" is also known as a comment,
+# these lines are ignored by the python interpreter even if
+# they contain code. The very first line is called a Shebang line, 
+# it is used to tell the system which interpreter to 
+# use(python2, python3, bash, etc). 
+
+# Author: Nithurshan Raveendran
+# Author ID: 188141212
+# Date Created: 2024/11/08
+
+class Student:
+
+    # Define the name and number when a student object is created, ex. student1 = Student('john', 025969102)
+    def __init__(self, name, number):
+        self.name = name
+        # Ensure the student number is stored as a string to handle TypeError
+        self.number = str(number)
+        self.courses = {}
+
+    # Return student name and number
+    def displayStudent(self):
+        return 'Student Name: ' + self.name + '\n' + 'Student Number: ' + self.number
+
+    # Add a new course and grade to student's record
+    def addGrade(self, course, grade):
+        self.courses[course] = grade
+
+    # Calculate the grade point average of all courses and return a string
+    def displayGPA(self):
+        # Avoid division by zero by checking the number of courses
+        if len(self.courses) == 0:
+            return 'GPA of student ' + self.name + ' is 0.0'
+        
+        # Calculate the GPA based on the grades
+        gpa = sum(self.courses.values()) / len(self.courses)
+        return 'GPA of student ' + self.name + ' is ' + str(gpa)
+
+    # Return a list of courses that the student passed (not a 0.0 grade)
+    def displayCourses(self):
+        passed_courses = [course for course, grade in self.courses.items() if grade > 0.0]
+        return passed_courses
+
+if __name__ == '__main__':
+    # Create first student object and add grades for each class
+    student1 = Student('John', '013454900')
+    student1.addGrade('uli101', 1.0)
+    student1.addGrade('ops245', 2.0)
+    student1.addGrade('ops445', 3.0)
+
+    # Create second student object and add grades for each class
+    student2 = Student('Jessica', 123456)
+    student2.addGrade('ipc144', 4.0)
+    student2.addGrade('cpp244', 3.5)
+    student2.addGrade('cpp344', 0.0)
+
+    # Display information for student1 object
+    print(student1.displayStudent())
+    print(student1.displayGPA())
+    print(student1.displayCourses())
+
+    # Display information for student2 object
+    print(student2.displayStudent())
+    print(student2.displayGPA())
+    print(student2.displayCourses())
